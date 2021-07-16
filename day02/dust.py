169c3c97f5c90f9a2e1b579507e675ec96802f43
이정원 <sorrow4468@gmail.com>
2021-07-16 17:22:42 +0900

    20210716

--- day02/dust.py
+++ day02/dust.py
@@ -1,9 +1,9 @@
 import requests
 from bs4 import BeautifulSoup
 
-sidoName = input("전국, 서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종 중에서 입력해주세요: ")
+KEY = "yFKV%2BECvAWDL2FspF6wVihm6%2FhemFqTsl9lcic0BCfp1fp2QUVc%2BexlgQ%2BHPsSLDTqkuYP3e%2BNYXPTWhkyvnWw%3D%3D"
 station_number = 1
-url = f"http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey=yFKV%2BECvAWDL2FspF6wVihm6%2FhemFqTsl9lcic0BCfp1fp2QUVc%2BexlgQ%2BHPsSLDTqkuYP3e%2BNYXPTWhkyvnWw%3D%3D&returnType=json&numOfRows=100&pageNo=1&sidoName={sidoName}%ver=1.0"
+url = f"http://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getCtprvnRltmMesureDnsty?serviceKey={KEY}&returnType=json&numOfRows=100&pageNo=1&sidoName=%EB%B6%80%EC%82%B0&ver=1.0"
 data = requests.get(url).json()
 dust = int(data["response"]["body"]["items"][station_number]["pm10Value"])
 station = str(data["response"]["body"]["items"][station_number]["stationName"])
