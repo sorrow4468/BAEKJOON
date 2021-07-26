6863930bec8109375b754952798426d91a22f996
이정원 <sorrow4468@gmail.com>
2021-07-26 09:00:14 +0900

    210726 | TIL test

--- startcamp/day02/test_requests.py
+++
@@ -1,9 +0,0 @@
-# 파이썬으로 요청 보내기 위한 준비
-# requests 라는 모듈을 사용 할거다.
-# 그러기 위해서 불러온다.
-import requests
-
-# requests로 https://www.naver.com 으로 요청 보낸 결과 출력
-print(requests.get("https://www.naver.com"))
-# alt + 방향키 위 아래로 한 줄 복사
-print(requests.get("https://www.naver.com").status_code)
