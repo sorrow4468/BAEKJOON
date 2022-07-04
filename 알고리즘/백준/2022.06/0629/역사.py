6978aa08543536ce48578124f2080050c3cc2ba3
정원 <sorrow4468@gmail.com>
2022-07-04 19:57:40 +0900

    백준 1613 역사 파이썬 코드클리닝

--- 알고리즘/백준/2022.06/0629/역사.py
+++ 알고리즘/백준/2022.06/0629/역사.py
@@ -1,22 +1,18 @@
-INF = int(0)
 N, M = map(int, input().split())
-arr = [[INF]*N for _ in range(N)]
+arr = [[0]*N for _ in range(N)]
 for m in range(M):
     a, b = map(int, input().split())
-    a, b = a-1, b-1
-    arr[a][b] = 1
+    arr[a-1][b-1] = 1
 for k in range(N):
     for a in range(N):
         for b in range(N):
             if arr[a][k] and arr[k][b]:
                 arr[a][b] = 1
-S = int(input())
-for s in range(S):
-    a, b = map(int, input().split())    
-    a, b = a-1, b-1
+for s in range(int(input())):
+    a, b = map(int, input().split())   
     result = 0
-    if arr[a][b]:
+    if arr[a-1][b-1]:
         result = -1
-    elif arr[b][a]:
+    elif arr[b-1][a-1]:
         result = 1
     print(result)
