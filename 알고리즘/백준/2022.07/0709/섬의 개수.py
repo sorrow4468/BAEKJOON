6da94e29fb465ca56c9838674a14f97f0fecb6c3
정원 <sorrow4468@gmail.com>
2022-07-10 11:39:29 +0900

    JJMG 스터디 주석 추가

--- 알고리즘/백준/2022.07/0709/섬의 개수.py
+++ 알고리즘/백준/2022.07/0709/섬의 개수.py
@@ -1,7 +1,13 @@
-from collections import deque
+"""
+배열을 탐색하다가 섬을 발견하면
+    섬의 개수 ++
+    섬을 바다로 만든다 (1 -> 0) # BFS (DFS로 하는데 메모리초과가 계속 남 ㅜㅜ)
+    발견한 섬의 개수를 출력
+"""
+from collections import deque # BFS용 double ended queue
 
 
-def bfs(y, x):
+def bfs(y, x): # 섬을 바다로 만드는 BFS함수
     Q.append((y, x))
     while Q:
         y, x = Q.popleft()
@@ -15,21 +21,21 @@ def bfs(y, x):
                 Q.append((ny, nx))
 
 
-dy = [-1, 1, 0, 0, 1, 1, -1, -1]
+dy = [-1, 1, 0, 0, 1, 1, -1, -1] # 대각선 포함한 8방향 델타이동
 dx = [0, 0, -1, 1, 1, -1, 1, -1]
 while True:
     W, H = map(int, input().split())
-    if W == 0 and H == 0:
+    if W == 0 and H == 0: # 테스트케이스 입력 종료조건
         break
-    arr = [list(map(int, input().split())) for _ in range(H)]
-    result = 0
+    arr = [list(map(int, input().split())) for _ in range(H)] # 지도
+    result = 0 # 섬의 개수
     Q = deque()
     for h in range(H):
         for w in range(W):
-            if arr[h][w]:
-                bfs(h, w)
-                result += 1
-    print(result)
+            if arr[h][w]: # 섬을 발견하면
+                bfs(h, w) # 섬을 바다로 만들기
+                result += 1 # 섬의 개수 ++
+    print(result) # 섬의 개수 출력
 
 
 # 아래는 재귀함수를 활용한 DFS로 풀었는데
