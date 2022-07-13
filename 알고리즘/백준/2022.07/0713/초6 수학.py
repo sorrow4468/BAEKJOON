24a93f5ef811b6943a26317a6a1d01fbe33312b4
정원 <sorrow4468@gmail.com>
2022-07-13 10:58:36 +0900

    백준 브론즈 2

---
+++ 알고리즘/백준/2022.07/0713/초6 수학.py
@@ -0,0 +1,13 @@
+def gcd(a, b):
+    while b > 0:
+        a, b = b, a%b
+    return a
+
+def lcm(a, b):
+    return a * b // gcd(a, b)
+
+
+T = int(input())
+for t in range(T):
+    a, b = map(int, input().split())
+    print(lcm(a, b), gcd(a, b))
