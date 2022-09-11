ed931522b60f54700cd660ff83ce76ff824d2f7c
정원 <sorrow4468@gmail.com>
2022-09-11 22:38:41 +0900

    백준 다이나믹 프로그래밍

---
+++ 알고리즘/백준/2022.09/0911/계단 오르기.py
@@ -0,0 +1,12 @@
+N = int(input())
+S = [0 for _ in range(301)] # stairs 
+dp = [0 for _ in range(301)] # 미리 선언하여 N<=3 일 때 에러를 방지함
+for i in range(N): S[i] = int(input())
+dp[0] = S[0]
+dp[1] = S[0] + S[1]
+dp[2] = max(S[1]+S[2], S[0]+S[2])
+for i in range(3, N):
+    A = dp[i-3] + S[i-1] + S[i]
+    B = dp[i-2] + S[i]
+    dp[i] = max(A, B)
+print(dp[N-1])
