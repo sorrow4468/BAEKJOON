ff171157333c263f95fd95e20114c8f50b5a179c
정원 <sorrow4468@gmail.com>
2022-09-12 21:29:01 +0900

    템플릿 DP 추가

---
+++ 알고리즘/[템플릿]/Dynamic Programming/파도반 수열.py
@@ -0,0 +1,7 @@
+dp = [1, 1, 1, 2, 2, 3] + [0]*97
+for i in range(5, 103): 
+    dp[i] = dp[i-5]+dp[i-1]
+for _ in range(int(input())): 
+    print(dp[int(input())-1])
+
+# https://www.acmicpc.net/problem/9461
---
+++ 알고리즘/[템플릿]/Dynamic Programming/포도주 시식.py
@@ -0,0 +1,14 @@
+N = int(input())
+wines = [0] * 10002
+dp = [0] * 10002
+for n in range(N): wines[n] = int(input())
+dp[0] = wines[0]
+dp[1] = max(dp[0], wines[0]+wines[1])
+dp[2] = max(dp[1], wines[1]+wines[2], wines[0]+wines[2])
+for i in range(3, N):
+    A = dp[i-3] + wines[i-1] + wines[i]
+    B = dp[i-2] + wines[i]
+    dp[i] = max(A, B, dp[i-1])
+print(max(dp))
+
+# https://www.acmicpc.net/problem/2156
