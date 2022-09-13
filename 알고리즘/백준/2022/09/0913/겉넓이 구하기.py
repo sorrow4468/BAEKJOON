95728815d5bcf905fd5e617bd8513f517d2a390f
정원 <sorrow4468@gmail.com>
2022-09-13 15:04:46 +0900

    백준 실버 2 기하학

---
+++ 알고리즘/백준/2022/09/0913/겉넓이 구하기.py
@@ -0,0 +1,23 @@
+dy, dx = [-1, 1, 0, 0], [0, 0, -1, 1]
+N, M = map(int, input().split())
+arr = [list(map(int, input().split())) for _ in range(N)]
+zero = 0
+result = 0
+for i in range(N):
+    for j in range(M):
+        if arr[i][j] == 0: 
+            zero += 1
+            continue
+        now = arr[i][j]
+        while now > 0:
+            face = 4 # 도형의 면
+            if now == arr[i][j]: face += 1
+            y, x = i, j
+            for k in range(4):
+                ny, nx = y+dy[k], x+dx[k]
+                if 0<=ny<N and 0<=nx<M:
+                    if arr[ny][nx] >= now:
+                        face -= 1
+            result += face
+            now -= 1    
+print(result + N*M - zero)
