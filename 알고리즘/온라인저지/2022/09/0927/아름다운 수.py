65af703e816f903d3fab14afb3f600d2cb2b3a06
정원 <sorrow4468@gmail.com>
2022-09-27 13:11:14 +0900

    백준 브론즈 2

---
+++ 알고리즘/온라인저지/2022/09/0927/아름다운 수.py
@@ -0,0 +1,9 @@
+# import sys
+
+# input = sys.stdin.readline
+
+# for _ in range(int(input().rstrip())):
+#     num = set(input().rstrip())
+#     print(len(num))
+
+for _ in [0]*int(input()):print(len(set(input())))
